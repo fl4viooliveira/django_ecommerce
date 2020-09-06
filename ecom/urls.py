d979100f0d215e005cfd62de44ec22c82d52a36f
fl4viooliveira <fl4viooliveira@gmail.com>
2020-09-06 11:33:54 +0100

    hello world setup

--- ecom/urls.py
+++ ecom/urls.py
@@ -4,8 +4,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.HomeView.as_view(), name='home')
 ]
 
 

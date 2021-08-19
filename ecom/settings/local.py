ac64e02d3580d6bb04488010dbda524ab81fdf04
Andrey Shaikin <34868233+kiwib0n@users.noreply.github.com>
2021-08-19 22:30:45 +0300

    Delete local.py

--- ecom/settings/local.py
+++
@@ -1,8 +0,0 @@
-from .main import *
-
-DATABASES: Dict[str, Dict[str, Any]] = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
